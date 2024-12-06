c2e646e0c0afc879a19993d7b3ea1a606b9c80dc
Sabbir Ahmed Talukdar <satsabbir11@gmail.com>
2024-12-06 22:01:39 +0600

    Update font and add Query Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,31 @@
-import {Inter} from 'next/font/google'
 import './globals.css'
-import {NextAuthProvider} from "@/providers";
-import {Toaster} from "react-hot-toast";
+import { NextAuthProvider } from "@/providers";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({subsets: ['latin']})
+//mona sans
+import { Work_Sans } from 'next/font/google'
+import TanstackQueryProvider from '@/provider/tanstack-query';
+const workSans = Work_Sans({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+})
 
 export const metadata = {
     title: 'Call for Blood',
     description: 'Call for Blood is a platform to connect blood donors with blood recipients.',
 }
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
-        <NextAuthProvider>
-            <Toaster />
-            {children}
-        </NextAuthProvider>
-        </body>
+            <body className={workSans.className}>
+                <NextAuthProvider>
+                    <TanstackQueryProvider>
+                        <Toaster />
+                        {children}
+                    </TanstackQueryProvider>
+                </NextAuthProvider>
+            </body>
         </html>
     )
 }
